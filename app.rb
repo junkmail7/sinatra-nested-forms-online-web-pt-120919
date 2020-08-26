e956d451d29d8e6485a1f05dcb5d2b13009590ba
William Pannell <lpann5@gmail.com>
2020-08-26 05:56:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,24 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :index
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :create_puppy
+    end
 
+    post '/pirate' do
+      @pirate = pirate.new(params[:pirate])
+     
+      params[:pirate][:ships].each do |details|
+        Course.new(details)
+      end
+     
+      @ships = Ship.all
+     
+      erb :pirate
+    end
   end
 end
