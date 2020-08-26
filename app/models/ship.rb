ff40d1df63d60c173befe15c6209329f5793911f
William Pannell <lpann5@gmail.com>
2020-08-26 05:49:42 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_reader :name, :weight, :height
+  @@all = []
+  def initialize(name,weight,height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def clear
+    @@all.delete_all
+  end
 end
